a6468148b6e203e562cfff0a84874fac85c1657c
Jacob Wright <jacwright@gmail.com>
2011-03-04 11:49:57 -0700

    Moving files into a src folder and adding compile scripts

---
+++ make.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+./compile.js -nc \
+	--combine src/admin/js/admin.js \
+	--compile src/admin/js/admin.min.js \
+	--path src/admin/js/libs \
+	--path src/admin/js/app \
+	--force-commonjs \
+	--main app \
+	src/admin/js/libs/jquery-1.5.1.min.js^ \
+	src/admin/js/app/pages/*
